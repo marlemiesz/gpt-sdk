e48516b5780068fda1426d9c431a7a15f51c2b6d
Marcin Lemiesz <mar.lemiesz@gmail.com>
2023-02-07 16:19:45 +0100

    Prepare test skeleton

--- src/Response/ResponseInterface.php
+++ src/Response/ResponseInterface.php
@@ -2,7 +2,18 @@
 
 namespace Marlemiesz\GptSdk\Response;
 
+
+use Marlemiesz\GptSdk\Entity\EntityInterface;
+
 interface ResponseInterface
 {
+    public function addItem(EntityInterface $item): void;
+    public function removeItem(int $index): void;
+    public function getItem(int $index): EntityInterface;
+    public function getItems(): array;
+    
+    public function countItems(): int;
+    
+    public function validate(): bool;
     
 }
---
+++ tests/OpenAiTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Marlemiesz\GptSdk;
+
+use PHPUnit\Framework\TestCase;
+
+class OpenAiTest extends TestCase
+{
+    
+}
