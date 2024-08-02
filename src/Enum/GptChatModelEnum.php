9c00f33cfa67f30584e1618411f4a74421f96a5f
caprisan <patrykkalinowski99@wp.pl>
2024-08-02 11:32:20 +0200

    adding new models: gpt-4o, gpt-4o-mini, gpt-4-turbo

--- src/Enum/GptChatModelEnum.php
+++ src/Enum/GptChatModelEnum.php
@@ -7,12 +7,18 @@ enum GptChatModelEnum
 
     case GPT35Turbo;
     case GPT4;
+    case GPT4o;
+    case GPT4oMINI;
+    case GPT4Turbo;
 
     public function getValue() : string
     {
         return match ($this) {
             self::GPT35Turbo => 'gpt-3.5-turbo',
             self::GPT4 => 'gpt-4',
+            self::GPT4o => 'gpt-4o',
+            self::GPT4oMINI => 'gpt-4o-mini',
+            self::GPT4Turbo => 'gpt-4-turbo',
 
         };
     }
